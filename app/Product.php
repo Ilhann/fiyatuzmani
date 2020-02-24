c4a6c0d78d5559cfa81a1538e1b5915fa5177a4d
Ilhan Mihaylov <ilhan.mihaylov@gmail.com>
2020-02-24 23:24:30 +0300

    Model Update: Product

--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
-    protected $fillable = ["productURL", "provider", "title", "last_receive"];
+    protected $fillable = ["productURL", "provider", "title", "last_receive", "productid", "source"];
 }
