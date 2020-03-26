586895071ec1f24478afa8790dbbdefbd375fab4
Ilhan Mihaylov <ilhan.mihaylov@gmail.com>
2020-03-26 14:27:19 +0300

    Search query param name update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function search_with_name(Request $request){
-        $querystring = $request->get('url');
+        $querystring = $request->get('query');
         $search_result = \App\Product::select("id", "title", "productURL")->where("title", "like", "%{$querystring}%")->get();
         return json_encode($search_result);
     }
