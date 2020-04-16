e71408d6073060e1692af8d29eccd08d000a00a5
Ilhan Mihaylov <ilhan.mihaylov@gmail.com>
2020-04-16 13:16:56 +0300

    Fixes For New Products

--- app/Http/Controllers/CrawlController.php
+++ app/Http/Controllers/CrawlController.php
@@ -10,7 +10,6 @@ class CrawlController extends Controller
     //
     public function add_new_hepsiburada(Request $request){
         $client = GetGoutteForCrawler();
-        $client->setHeader('User-Agent', $useragent);
         $crawler = $client->request('GET', $request->get('url'));
         $price = $crawler->filter('#offering-price')->attr('content');
         $name = $crawler->filter('#product-name')->text();
@@ -22,7 +21,8 @@ class CrawlController extends Controller
         $product->productURL = $request->get('url');
         $product->provider = "hepsiburada";
         $product->title = $name;
-        $product->last_receive = now()->subSeconds(3600);
+        $product->last_receive = now()->subSeconds(90000);
+        $product->last_dispatch = now()->subSeconds(90000);
         $product->source = "url";
         $product->productid = $crawler->filter('input[name=productId]')->attr('value');
         $product->save();
@@ -33,7 +33,6 @@ class CrawlController extends Controller
 
     public function add_new_trendyol(Request $request){
         $client = GetGoutteForCrawler();
-        $client->setHeader('User-Agent', $useragent);
         $crawler = $client->request('GET', $request->get('url'));
         $price = $crawler->filter('meta[name="twitter:data1"]')->attr('content');
         $name = $crawler->filter('.pr-nm')->text();
@@ -46,8 +45,8 @@ class CrawlController extends Controller
         $product->provider = "trendyol";
         $product->title = $name;
         $product->last_receive = now()->subSeconds(90000);
+        $product->last_dispatch = now()->subSeconds(90000);
         $product->source = "url";
-        //$product->productid = $crawler->filter('input[name=productId]')->attr('value');
         $product->save();
 
         return "[TRENDYOL] Product with ID: ". strval($product->id) ." successfully added to tracking table. Current Price: " . $price . ", Current Title: " . $name . "";
